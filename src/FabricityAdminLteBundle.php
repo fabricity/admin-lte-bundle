2d4767b5e7bab34620e70ed1a731fbfae9f55e5b
David Mattei <david.mattei@sappico.be>
2020-11-12 21:08:17 +0100

    feat: add sf bundle

---
+++ src/FabricityAdminLteBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fabricity\Bundle\AdminLteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FabricityAdminLteBundle extends Bundle
+{
+}
