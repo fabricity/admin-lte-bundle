1d3e20ee12c41eb6e912bbec64d8ecdd58948596
David Mattei <david.mattei@sappico.be>
2020-11-28 00:40:52 +0100

    feat: add login page template

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,24 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('./src/Resources/public/')
-    .setPublicPath('/bundles/adminlte/')
-    .setManifestKeyPrefix('bundles/admin-lte')
-
-    .addEntry('admin-lte', './assets/admin-lte.js')
-
-    .disableSingleRuntimeChunk()
+    .setPublicPath('./')
+    .setManifestKeyPrefix('bundles/fabricity-admin-lte')
 
     .cleanupOutputBeforeBuild()
     .enableSassLoader()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-
+    .enableSourceMaps(false)
+    .enableVersioning(false)
+    .disableSingleRuntimeChunk()
     .autoProvidejQuery()
+
+    // copy FontAwesome fonts
+    .copyFiles({
+        from: './node_modules/@fortawesome/fontawesome-free/webfonts/',
+        // relative to the output dir
+        to: 'fonts/[name].[hash].[ext]'
+    })
+
+    .addEntry('fabricity-admin-lte', './assets/admin-lte.js')
 
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
